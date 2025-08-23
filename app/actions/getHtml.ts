9fb110293a1d6d5d09cb06706a64e3099a2f17c9
Deniz Gunes <denizlg24@gmail.com>
2025-08-23 13:59:38 +0100

    Removed path joining

--- app/actions/getHtml.ts
+++ app/actions/getHtml.ts
@@ -1,14 +1,9 @@
 "use server"
 
 import { readFileSync } from "fs";
-import path from "path";
 
 export async function getHtml(url: string, replace: Record<string, string>[]) {
-    const htmlPath = path.join(
-        process.cwd(),
-        url
-    );
-    let html = readFileSync(htmlPath, "utf8");
+    let html = readFileSync(url, "utf8");
     for (const map of replace) {
         for (const [key, value] of Object.entries(map)) {
             const regex = new RegExp(key, "g");
