57a7817279940dcbdca695ae2ccebac08ce81bb8
Deniz Gunes <denizlg24@gmail.com>
2025-09-19 22:35:03 +0100

    Debugging email send

--- app/actions/sendMail.ts
+++ app/actions/sendMail.ts
@@ -3,27 +3,27 @@ import env from "@/utils/env";
 import nodemailer from "nodemailer";
 
 export async function sendMail({ email, html, subject }: { email: string, html: string, subject: string }) {
-    const transporter = nodemailer.createTransport({
-        host: "mail.alojamentoideal.pt",
-        port: 465,
-        secure: true, // upgrade later with STARTTLS
-        auth: {
-            user: "site@alojamentoideal.pt",
-            pass: env.WEBMAIL_PASS,
-        },
-    });
-    const mailOptions = {
-        from: "Alojamento Ideal <site@alojamentoideal.pt>",
-        to: email,
-        subject,
-        html,
-    };
-    transporter.sendMail(mailOptions, function (err) {
-        if (err) {
-            console.log("Mail failed: ",err);
-            return { success: false, message: "mail-error" };
-        }
-    });
-    console.log("Mail sent!")
-    return { success: true }
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "mail.alojamentoideal.pt",
+            port: 465,
+            secure: true, // upgrade later with STARTTLS
+            auth: {
+                user: "site@alojamentoideal.pt",
+                pass: env.WEBMAIL_PASS,
+            },
+        });
+        const mailOptions = {
+            from: "Alojamento Ideal <site@alojamentoideal.pt>",
+            to: email,
+            subject,
+            html,
+        };
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Mail sent: ", info.messageId);
+        return { success: true };
+    } catch (error) {
+        console.error("Mail failed: ", error);
+        return { success: false, message: "mail-error" };
+    }
 }
